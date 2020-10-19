0631adedb617797691b5bd7c4acad26092a7bb8d
rigel selmani <rigelrigelselmani@hotmail.com>
2020-10-19 04:11:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from "./Header"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>  
+        <Header />  
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header (){
+    return  <header>
+              <img className="head-img" src="https://rigelselmani.github.io/Word-Scape/assets/images/logo%20(2).png" alt="word-img"/>
+            </header> 
+}
+
+export default Header;
